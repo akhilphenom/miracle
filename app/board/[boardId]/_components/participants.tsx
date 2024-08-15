e6c298e9fc8ed5bc311875e7ff71de65031098c7
Sai Akhil Katukam <saiakhilk.katukam@gmail.com>
2024-08-15 15:00:19 +0530

    added animated tooltip

--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -1,10 +1,27 @@
+"use client";
+
 import { Skeleton } from '@/components/ui/skeleton'
 import React from 'react'
+import { AnimatedTooltip } from "@/components/ui/animated-tooltip";
+import { useOthers, useSelf } from '@liveblocks/react';
+
+const people = [
+  {
+    _id: 1,
+    name: "John Doe",
+    designation: "Software Engineer",
+    image: "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+  },
+];
 
 function Participants() {
+  const {} = useOthers();
+
   return (
-    <div className='shadow-md bg-white absolute top-3 right-3 px-3 py-2 text-sm rounded-md'>
-      Participants
+    <div className='absolute bottom-3 right-[50%] translate-x-1/2'>
+      <div className="flex flex-row items-center justify-center w-full">
+        <AnimatedTooltip items={people} height={40} width={40} />
+      </div>
     </div>
   )
 }
